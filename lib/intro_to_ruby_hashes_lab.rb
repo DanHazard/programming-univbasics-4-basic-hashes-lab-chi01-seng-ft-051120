8972ad47f44d9c7f8bfce2e3f29753a851ce9815
Dan Love <daniel.love.jr@gmail.com>
2020-04-29 21:58:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require pry
+
 def new_hash
   # return an empty hash
   {}
@@ -23,5 +25,6 @@ end
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
   id_hash = {}
-  id_hash[id:] = number
+  binding.pry
+  id_hash[:id] = number
 end
