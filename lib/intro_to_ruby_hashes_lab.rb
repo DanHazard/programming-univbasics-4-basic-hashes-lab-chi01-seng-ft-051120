e3eb40516f9718bd26a332c5bd86754650f2ee02
Dan Love <daniel.love.jr@gmail.com>
2020-04-29 21:54:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  pioneer_hash = {
-    :name "Grace Hopper"
+  {
+    :name => "Grace Hopper"
   }
 end
 
